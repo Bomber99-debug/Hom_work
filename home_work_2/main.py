921bd96d48042a1a6295ff9bc89ff7b3237185a9
Bomber99-debug <andreyrybchenko16@gmail.com>
2026-02-06 18:00:00 +0200

    appdate home work 2

--- home_work_2/main.py
+++ home_work_2/main.py
@@ -1,1 +1,5 @@
-print("1")
+from progres import total_salary
+
+FILE_NAME = 'info.txt'
+total, average  = total_salary(FILE_NAME)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
